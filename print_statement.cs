381106373f9110071885b8e46c4e4555b68f065e
jamesmsadhusundar <126789300+jamesmsadhusundar@users.noreply.github.com>
2023-03-09 23:40:21 +0530

    Add files via upload

---
+++ print_statement.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+namespace print_statement
+{
+    class Program
+    {
+        static void Main (String[] args)
+        {
+            Console.WriteLine("hello there");
+
+            Console.Beep(); //generates beep sound 
+        }
+    }
+}
