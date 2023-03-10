76caa59ba6c463296cc75528279dccd5cb7d308a
jamesmsadhusundar <126789300+jamesmsadhusundar@users.noreply.github.com>
2023-03-10 17:27:29 +0530

    Add files via upload

---
+++ print_statement_2.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace print_statement_2
+{
+    class Program
+    {
+        static void Main (String[] args)
+        {
+            //single-line comment
+
+
+
+            /*
+             
+             multi-line comment
+             
+            */
+
+            Console.Write("james ");
+            Console.Write("m.sadhu ");
+            Console.WriteLine("sundar");
+
+            Console.ReadLine(); //will not stop output until getting return value
+        }
+    }
+}
