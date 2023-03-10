8cd53cc0c7ad400b7cbcac71d216d4a3b34e0354
jamesmsadhusundar <126789300+jamesmsadhusundar@users.noreply.github.com>
2023-03-10 18:12:12 +0530

    Add files via upload

---
+++ variables.cs
@@ -0,0 +1,29 @@
+class Program
+{
+    static void Main (String[] args)
+    {
+        String name = "james";
+        
+        int age = 19;
+
+        double height = 185.25;
+
+        char c = '₹';
+
+        Console.WriteLine("******************************");
+
+        Console.WriteLine("            PROFILE           ");
+
+        Console.WriteLine("");
+
+        Console.WriteLine("name    :    "+name);
+
+        Console.WriteLine("age     :    "+age);
+
+        Console.WriteLine("height  :    "+height+"cm");
+
+        Console.WriteLine("salary  :    40000"+c);
+
+        Console.WriteLine("******************************");
+    }
+}
