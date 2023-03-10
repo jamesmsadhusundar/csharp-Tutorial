bdb48a186fbf73ac1e5030ca349bc8366cda325b
jamesmsadhusundar <126789300+jamesmsadhusundar@users.noreply.github.com>
2023-03-10 18:32:31 +0530

    Add files via upload

---
+++ constant.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace constant
+{
+    class Program
+    {
+        static void Main (String[] args)
+        {
+            const String name = "james";   //const is a keyword, it declares variable as constant which is never changeable.
+            
+            Console.WriteLine(name);
+        }
+    }
+}
